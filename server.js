f7e9aa4c7c50f87bcd7aef68f4c82081b4e9094d
eosocram <socramlmonteiro@gmail.com>
2023-08-26 08:33:36 -0300

    inclui type modules e criei mais uma requisição GET usando um dado mock

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './src/app.js';
-const port = 3000;
+const PORT = 3000;
 
-app.listen(port, () => {
-    console.log(`O servidor está rodando no endereço http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`O servidor está rodando no endereço http://localhost:${PORT}`)
 });
--- src/app.js
+++ src/app.js
@@ -3,11 +3,22 @@ import express from 'express';
 
 const app = express();
 
+//mock
+const selecoes = [
+    {id: 1, selecao: 'Brasil', grupo: 'G'},
+    {id: 2, selecao: 'Suiça', grupo: 'G'},
+    {id: 3, selecao: 'Sérvia', grupo: 'G'},
+    {id: 4, selecao: 'Camarões', grupo: 'G'},
+];
 
 // rota raíz
 app.get('/', (req, res) =>{
     res.send('O servidor js está rodando.')
 }); 
+
+app.get('/selecoes', (req, res) => {
+    res.status(200).send(selecoes)
+})
 
 export default app;
 
